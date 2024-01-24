3434de167d4ee0d8ec2b328c7970813777542c08
Warren Hong <hwhong1129@gmail.com>
2024-01-23 22:13:26 -0800

    feat: inclusion of chat prompt

--- src/pages/api/in-memory-rag.ts
+++ src/pages/api/in-memory-rag.ts
@@ -4,6 +4,10 @@ import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 import { MemoryVectorStore } from "langchain/vectorstores/memory";
 import { RunnableSequence } from "@langchain/core/runnables";
 import { convertDocsToString, loadAndSplitDocs } from "@/utils/helper";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { RunnableMap } from "@langchain/core/runnables";
+import { ChatOpenAI } from "@langchain/openai";
+import { StringOutputParser } from "@langchain/core/output_parsers";
 
 export default async function handler(
   req: NextApiRequest,
@@ -30,9 +34,46 @@ export default async function handler(
     convertDocsToString,
   ]);
 
-  const results = await documentRetrievalChain.invoke({
+  // const results = await documentRetrievalChain.invoke({
+  //   question: "What are the prerequisites for this course?",
+  // });
+
+  const TEMPLATE_STRING = `You are an experienced researcher, 
+expert at interpreting and answering questions based on provided sources.
+Using the provided context, answer the user's question 
+to the best of your ability using only the resources provided. 
+Be verbose!
+
+<context>
+
+{context}
+
+</context>
+
+Now, answer this question using the above context:
+
+{question}`;
+
+  const answerGenerationPrompt =
+    ChatPromptTemplate.fromTemplate(TEMPLATE_STRING);
+
+  const model = new ChatOpenAI({
+    modelName: "gpt-3.5-turbo-1106",
+  });
+
+  const retrievalChain = RunnableSequence.from([
+    {
+      context: documentRetrievalChain,
+      question: (input) => input.question,
+    },
+    answerGenerationPrompt,
+    model,
+    new StringOutputParser(),
+  ]);
+
+  const answer = await retrievalChain.invoke({
     question: "What are the prerequisites for this course?",
   });
 
-  res.status(200).json({ result: results });
+  res.status(200).json({ result: answer });
 }
