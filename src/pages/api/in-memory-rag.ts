fc449720942668317fdff52dc9b8a3ab86e3f615
Warren Hong <hwhong1129@gmail.com>
2024-01-24 21:31:27 -0800

    feat: able to retain history of the conversation

--- src/pages/api/in-memory-rag.ts
+++ src/pages/api/in-memory-rag.ts
@@ -8,6 +8,10 @@ import { ChatPromptTemplate } from "@langchain/core/prompts";
 import { RunnableMap } from "@langchain/core/runnables";
 import { ChatOpenAI } from "@langchain/openai";
 import { StringOutputParser } from "@langchain/core/output_parsers";
+import { MessagesPlaceholder } from "@langchain/core/prompts";
+import { RunnablePassthrough } from "@langchain/core/runnables";
+import { RunnableWithMessageHistory } from "@langchain/core/runnables";
+import { ChatMessageHistory } from "@langchain/community/stores/message/in_memory";
 
 export default async function handler(
   req: NextApiRequest,
@@ -34,46 +38,127 @@ export default async function handler(
     convertDocsToString,
   ]);
 
-  // const results = await documentRetrievalChain.invoke({
-  //   question: "What are the prerequisites for this course?",
+  /** ---------- PART II ------------ */
+
+  // const TEMPLATE_STRING = `You are an experienced researcher,
+  // expert at interpreting and answering questions based on provided sources.
+  // Using the provided context, answer the user's question
+  // to the best of your ability using only the resources provided.
+  // Be verbose!
+
+  // <context>
+
+  // {context}
+
+  // </context>
+
+  // Now, answer this question using the above context:
+
+  // {question}`;
+
+  // const answerGenerationPrompt =
+  //   ChatPromptTemplate.fromTemplate(TEMPLATE_STRING);
+
+  // const model = new ChatOpenAI({
+  //   modelName: "gpt-3.5-turbo-1106",
   // });
 
-  const TEMPLATE_STRING = `You are an experienced researcher, 
+  // const retrievalChain = RunnableSequence.from([
+  //   {
+  //     context: documentRetrievalChain,
+  //     question: (input) => input.question,
+  //   },
+  //   answerGenerationPrompt,
+  //   model,
+  //   new StringOutputParser(),
+  // ]);
+
+  // const answer = await retrievalChain.invoke({
+  //   question: "What is the assignment late policy?",
+  // });
+
+  /** ---------- PART II ------------ */
+
+  const REPHRASE_QUESTION_SYSTEM_TEMPLATE = `Given the following conversation and a follow up question, 
+rephrase the follow up question to be a standalone question.`;
+
+  const rephraseQuestionChainPrompt = ChatPromptTemplate.fromMessages([
+    ["system", REPHRASE_QUESTION_SYSTEM_TEMPLATE],
+    new MessagesPlaceholder("history"),
+    [
+      "human",
+      "Rephrase the following question as a standalone question:\n{question}",
+    ],
+  ]);
+
+  const ANSWER_CHAIN_SYSTEM_TEMPLATE = `You are an experienced researcher, 
 expert at interpreting and answering questions based on provided sources.
-Using the provided context, answer the user's question 
-to the best of your ability using only the resources provided. 
-Be verbose!
+Using the below provided context and chat history, 
+answer the user's question to the best of 
+your ability 
+using only the resources provided. Be verbose!
 
 <context>
-
 {context}
+</context>`;
 
-</context>
+  const answerGenerationChainPrompt = ChatPromptTemplate.fromMessages([
+    ["system", ANSWER_CHAIN_SYSTEM_TEMPLATE],
+    new MessagesPlaceholder("history"),
+    [
+      "human",
+      "Now, answer this question using the previous context and chat history:\n{standalone_question}",
+    ],
+  ]);
 
-Now, answer this question using the above context:
-
-{question}`;
-
-  const answerGenerationPrompt =
-    ChatPromptTemplate.fromTemplate(TEMPLATE_STRING);
-
-  const model = new ChatOpenAI({
-    modelName: "gpt-3.5-turbo-1106",
-  });
-
-  const retrievalChain = RunnableSequence.from([
-    {
-      context: documentRetrievalChain,
-      question: (input) => input.question,
-    },
-    answerGenerationPrompt,
-    model,
+  const rephraseQuestionChain = RunnableSequence.from([
+    rephraseQuestionChainPrompt,
+    new ChatOpenAI({ temperature: 0.1, modelName: "gpt-3.5-turbo-1106" }),
     new StringOutputParser(),
   ]);
 
-  const answer = await retrievalChain.invoke({
-    question: "What are the prerequisites for this course?",
+  const conversationalRetrievalChain = RunnableSequence.from([
+    RunnablePassthrough.assign({
+      standalone_question: rephraseQuestionChain,
+    }),
+    RunnablePassthrough.assign({
+      context: documentRetrievalChain,
+    }),
+    answerGenerationChainPrompt,
+    new ChatOpenAI({ modelName: "gpt-3.5-turbo" }),
+    new StringOutputParser(),
+  ]);
+
+  const messageHistory = new ChatMessageHistory();
+
+  const finalRetrievalChain = new RunnableWithMessageHistory({
+    runnable: conversationalRetrievalChain,
+    getMessageHistory: (_sessionId) => messageHistory,
+    historyMessagesKey: "history",
+    inputMessagesKey: "question",
   });
 
-  res.status(200).json({ result: answer });
+  const originalQuestion = "What is the assignment late policy?";
+
+  // const originalAnswer = await finalRetrievalChain.invoke(
+  //   {
+  //     question: originalQuestion,
+  //   },
+  //   {
+  //     configurable: { sessionId: "test" },
+  //   }
+  // );
+
+  const finalResult = await finalRetrievalChain.invoke(
+    {
+      question: "Can you list them in bullet point form?",
+    },
+    {
+      configurable: { sessionId: "test" },
+    }
+  );
+
+  console.log(finalResult);
+
+  res.status(200).json({ finalResult });
 }
