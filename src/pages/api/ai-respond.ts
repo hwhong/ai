62df23653125af3ba86ce89fea6720131dc15fad
Warren Hong <hwhong1129@gmail.com>
2024-03-02 21:29:28 -0800

    feat: finally works with memory

--- src/pages/api/ai-respond.ts
+++ src/pages/api/ai-respond.ts
@@ -1,57 +1,36 @@
 import { CONVERSATION_SYSTEM_PROMPT } from "@/utils/conversation/prompt";
-import { chatModel } from "@/utils/model";
+import { mode } from "@/utils/model";
 import {
   ChatPromptTemplate,
   HumanMessagePromptTemplate,
   MessagesPlaceholder,
   SystemMessagePromptTemplate,
-} from "@langchain/core/prompts";
+} from "langchain/prompts";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { BufferMemory } from "langchain/memory";
-import { RunnableSequence } from "@langchain/core/runnables";
+import { ConversationChain } from "langchain/chains";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
-  const msgs = req.body.messages;
-  const message = msgs[msgs.length - 1].content;
+  const messages = req.body.messages;
+  const userMessage = messages[messages.length - 1].content;
 
-  const prompt = ChatPromptTemplate.fromMessages([
+  const prompt = ChatPromptTemplate.fromPromptMessages([
     SystemMessagePromptTemplate.fromTemplate(CONVERSATION_SYSTEM_PROMPT),
     new MessagesPlaceholder("history"),
     HumanMessagePromptTemplate.fromTemplate("{input}"),
   ]);
 
-  const memory = new BufferMemory({
-    returnMessages: true,
-    inputKey: "input",
-    outputKey: "output",
-    memoryKey: "history",
+  const conversation_chain = new ConversationChain({
+    memory: new BufferMemory({ returnMessages: true, memoryKey: "history" }),
+    prompt: prompt,
+    llm: mode,
   });
 
-  const chain = RunnableSequence.from([
-    {
-      input: (initialInput) => initialInput?.input ?? "",
-      memory: () => memory.loadMemoryVariables({}),
-    },
-    {
-      input: (previousOutput) => previousOutput.input,
-      history: (previousOutput) => previousOutput.memory.history,
-    },
-    prompt,
-    chatModel,
-  ]);
+  const inputs1 = { input: userMessage };
+  const { response } = await conversation_chain.call(inputs1);
 
-  const inputs1 = { input: message };
-  const s1 = await chain.invoke(inputs1);
-
-  await memory.saveContext(inputs1, {
-    output: s1.content,
-  });
-
-  const inputs2 = { input: "quel est mon nom?" };
-  const s2 = await chain.invoke(inputs2);
-
-  res.status(200).json({ output: s2 });
+  res.status(200).json(response);
 }
